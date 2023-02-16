f7c70b067004efe8d5ee2da6fa754577b4830651
marcensie <aquila19908@gmail.com>
2023-02-16 13:54:15 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -141,107 +141,10 @@ fun ExercisePage(/*navController: NavController*/) {
             number = "30",
             ContDesc = "push up image")
 
-        //timer
-        Column(
-        modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-        Box(modifier = Modifier.size(200.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            var totalTime = 10L * 1000L
-            val  handleColor = Color(0xFF003355)
-            val inactiveBarColor = Color.DarkGray
-            val activeBarColor = Color(0xFF0097FC)
-            val  modifier = Modifier.size(200.dp)
-            val initialValue: Float = 1f
-            val strokeWidth: Dp = 5.dp
-            var size by remember { mutableStateOf(IntSize.Zero) }
-            var value by remember { mutableStateOf(initialValue) }
-            var currentTime by remember { mutableStateOf(totalTime) }
-            var isTimerRunning by remember { mutableStateOf(false) }
+timer(totalTime = 10L * 1000L)
 
-            LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
-                if(currentTime > 0 && isTimerRunning) {
-                    delay(100L)
-                    currentTime -= 100L
-                    value = currentTime / totalTime.toFloat()
-                }
-            }
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = modifier
-                    .onSizeChanged {
-                        size = it
-                    }
-            ) {
-                Canvas(modifier = modifier) {
-                    drawArc(
-                        color = inactiveBarColor,
-                        startAngle = -215f,
-                        sweepAngle = 250f,
-                        useCenter = false,
-                        size = Size(size.width.toFloat(), size.height.toFloat()),
-                        style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-                    )
-                    drawArc(
-                        color = activeBarColor,
-                        startAngle = -215f,
-                        sweepAngle = 250f * value,
-                        useCenter = false,
-                        size = Size(size.width.toFloat(), size.height.toFloat()),
-                        style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-                    )
-                    val center = Offset(size.width / 2f, size.height / 2f)
-                    val beta = (250f * value + 145f) * (PI / 180f).toFloat()
-                    val r = size.width / 2f
-                    val a = cos(beta) * r
-                    val b = sin(beta) * r
-                    drawPoints(
-                        listOf(Offset(center.x + a, center.y + b)),
-                        pointMode = PointMode.Points,
-                        color = handleColor,
-                        strokeWidth = (strokeWidth * 3f).toPx(),
-                        cap = StrokeCap.Round
-                    )
-                }
-                Text(
-                    text = (currentTime / 1000L).toString(),
-                    fontSize = 44.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-                Button(
-                    onClick = {
-                        if(currentTime <= 0L) {
-                            currentTime = totalTime
-                            isTimerRunning = true
-                        } else {
-                            isTimerRunning = !isTimerRunning
-                        }
-                    },
-                    modifier = Modifier.align(Alignment.BottomCenter),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
-                            Color(0xFF0067E4)
-                        } else {
-                            Color.Red
-                        }
-                    )
-                ) {
-                    Text(
-                        text = if (isTimerRunning && currentTime >= 0L) "Stop"
-                        else if (!isTimerRunning && currentTime >= 0L) "Start"
-                        else "Restart"
-                    )
-                }
-            }
-        }
-    }
-        //timer end
     }
 }
-
 
 @Composable
 fun topbar() {
@@ -371,4 +274,99 @@ fun exercise(imageId: Int,title: String,number: String,ContDesc: String) {
             )
         }
     }
+}
+
+@Composable
+fun timer(totalTime: Long){
+Column(
+modifier = Modifier.fillMaxSize(),
+horizontalAlignment = Alignment.CenterHorizontally
+){
+    Box(modifier = Modifier.size(200.dp),
+        contentAlignment = Alignment.Center
+    ) {
+       // var totalTime = 10L * 1000L
+        val initialValue: Float = 1f
+        var size by remember { mutableStateOf(IntSize.Zero) }
+        var value by remember { mutableStateOf(initialValue) }
+        var currentTime by remember { mutableStateOf(totalTime) }
+        var isTimerRunning by remember { mutableStateOf(false) }
+
+        LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
+            if(currentTime > 0 && isTimerRunning) {
+                delay(100L)
+                currentTime -= 100L
+                value = currentTime / totalTime.toFloat()
+            }
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(200.dp)
+                .onSizeChanged {
+                    size = it
+                }
+        ) {
+            Canvas(modifier = Modifier.size(200.dp)) {
+                drawArc(
+                    color = Color.DarkGray,
+                    startAngle = -215f,
+                    sweepAngle = 250f,
+                    useCenter = false,
+                    size = Size(size.width.toFloat(), size.height.toFloat()),
+                    style = Stroke(5.dp.toPx(), cap = StrokeCap.Round)
+                )
+                drawArc(
+                    color = Color(0xFF0097FC),
+                    startAngle = -215f,
+                    sweepAngle = 250f * value,
+                    useCenter = false,
+                    size = Size(size.width.toFloat(), size.height.toFloat()),
+                    style = Stroke(5.dp.toPx(), cap = StrokeCap.Round)
+                )
+                val center = Offset(size.width / 2f, size.height / 2f)
+                val beta = (250f * value + 145f) * (PI / 180f).toFloat()
+                val r = size.width / 2f
+                val a = cos(beta) * r
+                val b = sin(beta) * r
+                drawPoints(
+                    listOf(Offset(center.x + a, center.y + b)),
+                    pointMode = PointMode.Points,
+                    color = Color(0xFF003355),
+                    strokeWidth = (5.dp * 3f).toPx(),
+                    cap = StrokeCap.Round
+                )
+            }
+            Text(
+                text = (currentTime / 1000L).toString(),
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Button(
+                onClick = {
+                    if(currentTime <= 0L) {
+                        currentTime = totalTime
+                        isTimerRunning = true
+                    } else {
+                        isTimerRunning = !isTimerRunning
+                    }
+                },
+                modifier = Modifier.align(Alignment.BottomCenter),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
+                        Color(0xFF0067E4)
+                    } else {
+                        Color.Red
+                    }
+                )
+            ) {
+                Text(
+                    text = if (isTimerRunning && currentTime >= 0L) "Stop"
+                    else if (!isTimerRunning && currentTime >= 0L) "Start"
+                    else "Restart"
+                )
+            }
+        }
+    }
+}
 }
